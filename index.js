29814b2b125f643e44ec7a15d6d84823f200b953
Sonukr325 <56910117+Sonukr325@users.noreply.github.com>
2023-01-30 18:32:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const puppeteer = require("puppeteer-core");
 const app = express()
+const {executablePath} = require('puppeteer-core')
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+let options = {headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-gpu",
+      '--disable-setuid-sandbox'
+    ],
+    ignoreDefaultArgs: ['--disable-extensions']};
+   try {
+    let browser = await puppeteer.launch(options);
+
+    let page = await browser.newPage();
+    await page.goto("https://www.google.com");
+    res.send(await page.title());
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 })
 app.listen(process.env.PORT || 3000)
