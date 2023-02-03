2c6cc0f936fe11fecd18219e113fe5aabce703bb
Sonukr325 <56910117+Sonukr325@users.noreply.github.com>
2023-02-03 14:33:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const app = express()
 const {executablePath} = require('puppeteer-core')
 app.all('/', async (req, res) => {
